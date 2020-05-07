972acf97eb8ba6c294d320be1d672bc55f3bdf7b
KhrystynaKo <postforimportantthings@gmail.com>
2020-05-07 13:05:05 +0300

    change user's hand img

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ const endGame = document.querySelector("#end");
 const header = document.querySelector(".header");
 const game = document.querySelector(".game");
 
+const options = document.querySelectorAll(".option");
+
+const userHand = document.querySelector(".game-hands__user");
+const computerHand = document.querySelector(".game-hands__computer");
+
 let hideShowScreen = () => {
   header.classList.toggle("hide");
   game.classList.toggle("hide");
 };
 
 startGame.addEventListener("click", hideShowScreen);
+
+options.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    userHand.src = `img/${e.target.textContent}.png`;
+  })
+);
 
 endGame.addEventListener("click", hideShowScreen);
