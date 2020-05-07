c83791931d09733131f4d9a6c108b9bdc8566c27
KhrystynaKo <postforimportantthings@gmail.com>
2020-05-07 13:29:27 +0300

    change computer's hand img by random choise

--- main.js
+++ main.js
@@ -13,12 +13,25 @@ let hideShowScreen = () => {
   game.classList.toggle("hide");
 };
 
+let playerChoise = (e) => {
+  userHand.src = `img/${e.target.textContent}.png`;
+  let compChoice = computerChoise();
+  computerHand.src = `img/${compChoice}.png`;
+};
+
+let computerChoise = () => {
+  switch (Math.floor(1 + Math.random() * (3 + 1 - 1))) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+};
+
 startGame.addEventListener("click", hideShowScreen);
 
-options.forEach((item) =>
-  item.addEventListener("click", (e) => {
-    userHand.src = `img/${e.target.textContent}.png`;
-  })
-);
+options.forEach((item) => item.addEventListener("click", playerChoise));
 
 endGame.addEventListener("click", hideShowScreen);
